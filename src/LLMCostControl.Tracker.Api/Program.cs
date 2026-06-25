eed933df3550f0ea75185f0f5407d1033ee2eb3d
dev <dev@local>
2026-06-25 11:27:28 +0300

    M8: configure Orleans silo with PostgreSQL storage + memory streams, add storage SQL scripts

--- src/LLMCostControl.Tracker.Api/Program.cs
+++ src/LLMCostControl.Tracker.Api/Program.cs
@@ -1,9 +1,44 @@
+using LLMCostControl.Grains.Implementations;
 using LLMCostControl.Observability;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Orleans;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Host.UseObservability("LLMCostControl.Tracker.Api");
 builder.ConfigureObservability("LLMCostControl.Tracker.Api");
+
+var orleansConfig = builder.Configuration.GetSection("Orleans");
+var adoInvariant = "Npgsql";
+var connectionString = orleansConfig["StorageConnectionString"];
+
+builder.Host.UseOrleans(silo =>
+{
+    silo.ConfigureLogging(logging => logging.AddFilter("Orleans", LogLevel.Warning));
+
+    if (!string.IsNullOrWhiteSpace(connectionString))
+    {
+        silo.AddAdoNetGrainStorage("Default", options =>
+        {
+            options.ConnectionString = connectionString;
+            options.Invariant = adoInvariant;
+        });
+    }
+    else
+    {
+        silo.AddMemoryGrainStorage("Default");
+    }
+
+    silo.AddMemoryStreams("pricing");
+
+    silo.ConfigureServices(services =>
+    {
+        services.AddSingleton<PricingGrain>();
+        services.AddSingleton<UserBudgetGrain>();
+    });
+});
 
 var app = builder.Build();
 
